03a781b75847dad262736e10f3ab281e8a77ebba
Vaibhav Patel <vaibhav.p2305@gmail.com>
2023-01-04 23:19:27 -0500

    Added to github pages

---
+++ build/precache-manifest.8ff3fe1fd12b6fa706ec3fa33beeb332.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f09bbf8e6f5244a5b6e9387fabb33e",
+    "url": "/Ethereum-Dapp/index.html"
+  },
+  {
+    "revision": "56dfbc1bc565724bbd7e",
+    "url": "/Ethereum-Dapp/static/css/main.8db618ed.chunk.css"
+  },
+  {
+    "revision": "c89f1206ee015b1b98fc",
+    "url": "/Ethereum-Dapp/static/js/2.70829679.chunk.js"
+  },
+  {
+    "revision": "56dfbc1bc565724bbd7e",
+    "url": "/Ethereum-Dapp/static/js/main.896e0228.chunk.js"
+  },
+  {
+    "revision": "7c1ef806483959e39e55",
+    "url": "/Ethereum-Dapp/static/js/runtime-main.f8c7a698.js"
+  }
+]);
