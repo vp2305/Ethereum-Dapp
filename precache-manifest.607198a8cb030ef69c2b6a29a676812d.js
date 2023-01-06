4c718174c51cf4f72c2079d5a68cced218d0456e
Vaibhav Patel <vaibhav.p2305@gmail.com>
2023-01-06 12:37:16 -0500

    Updates

--- precache-manifest.607198a8cb030ef69c2b6a29a676812d.js
+++ precache-manifest.607198a8cb030ef69c2b6a29a676812d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c4422926e87600167aff0ba7a970a9c4",
+    "revision": "b1584c2cf4448366628af2e04950ba7e",
     "url": "/Ethereum-Dapp/index.html"
   },
   {
-    "revision": "b24cf17f07c1f7bda318",
+    "revision": "7ba6e90d5fae403b42d9",
     "url": "/Ethereum-Dapp/static/css/main.8db618ed.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Ethereum-Dapp/static/js/2.3611f56b.chunk.js"
   },
   {
-    "revision": "b24cf17f07c1f7bda318",
-    "url": "/Ethereum-Dapp/static/js/main.48ca7a13.chunk.js"
+    "revision": "7ba6e90d5fae403b42d9",
+    "url": "/Ethereum-Dapp/static/js/main.4b04bc95.chunk.js"
   },
   {
     "revision": "7c1ef806483959e39e55",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Ethereum-Dapp/precache-manifest.7a71120f41fe452aa550d4ce6be8c9be.js"
+  "/Ethereum-Dapp/precache-manifest.607198a8cb030ef69c2b6a29a676812d.js"
 );
 
 self.addEventListener('message', (event) => {
